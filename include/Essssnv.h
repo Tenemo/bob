ad6bf16af98a90eba3016fae3bda2a5abb7e7f3e
Piotr Piech <tenemo@gmail.com>
2024-11-22 08:36:48 +0100

    capitalization fixes for file names WIP

---
+++ include/Essssnv.h
@@ -0,0 +1,15 @@
+#ifndef ENV_H
+#define ENV_H
+
+/**
+ * @brief WiFi SSID for the network.
+ */
+#define WIFI_SSID "BestNetwork"
+// #define WIFI_SSID "Piotr_Wifi";
+
+/**
+ * @brief Password for the WiFi network.
+ */
+#define WIFI_PASSWORD "samsunggalaxys2"
+
+#endif

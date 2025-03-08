0ba71a184d8d084e0993e2066b15883791ca336d
Piotr Piech <tenemo@gmail.com>
2025-03-08 18:05:58 +0100

    redirect tweaks

--- bob-client/src/features/BobApi/bobApi.ts
+++ bob-client/src/features/BobApi/bobApi.ts
@@ -57,7 +57,7 @@ const dynamicBaseQuery: BaseQueryFn<
     }
 
     const urlEnd = typeof args === 'string' ? args : args.url;
-    const adjustedUrl = `https://bob.city/bob/${urlEnd}`;
+    const adjustedUrl = `http://${ip}/${urlEnd}`;
     const adjustedArgs =
         typeof args === 'string' ? adjustedUrl : { ...args, url: adjustedUrl };
 

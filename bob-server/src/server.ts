26cc6a50d5962f78a33f3966fb9215c1f84d245e
Piotr Piech <tenemo@gmail.com>
2025-03-15 20:21:35 +0100

    server redundant files removed

--- bob-server/src/server.ts
+++ bob-server/src/server.ts
@@ -1,71 +1,6 @@
-import express from 'express'; // ErrorRequestHandler
-// import {ErrorRequestHandler } from 'express';
-import cookieParser from 'cookie-parser';
-import { router } from 'routes/router';
-import { config } from './config';
-import cors from 'cors';
-import { setupLogging } from './logging';
 import axios from 'axios';
 
-export const app = express();
-
 const BOB_IP = '192.168.212.23';
-
-app.use(cors());
-app.use(express.json());
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-);
-app.use(cookieParser());
-
-const {
-    errorLogger,
-    logger,
-    // sentryErrorHandler
-} = setupLogging(app);
-
-app.use('/api', router);
-
-// The error handler must be BEFORE any other error middleware and AFTER all controllers
-// app.use(sentryErrorHandler);
-
-// Optional fallthrough error handler
-// const onErrorSentry: ErrorRequestHandler = (_err, _req, res) => {
-//     // The error id is attached to `res.sentry` to be returned
-//     // and optionally displayed to the user for support.
-//     res.statusCode = 500;
-//     // @ts-ignore
-//     res.end((res.sentry as string) + '\n');
-// };
-
-// app.use(onErrorSentry);
-
-// express-winston errorLogger AFTER the router and AFTER sentry.
-app.use(errorLogger);
-
-console.log = (message: string) =>
-    logger.log({
-        level: 'info',
-        message,
-    });
-console.warn = (message: string) =>
-    logger.warn({
-        level: 'warn',
-        message,
-    });
-console.error = (message: string) =>
-    logger.error({
-        level: 'error',
-        message,
-    });
-
-// export const server = app.listen(config.port, () => {
-//     console.log(
-//         `Server running on port ${config.port} in ${config.env} environment`,
-//     );
-// });
 
 const delay = (ms: number): Promise<void> =>
     new Promise((resolve) => setTimeout(resolve, ms));
@@ -112,12 +47,6 @@ const move = async ({ type }: { type: string }): Promise<void> => {
     }
 };
 
-const standUp = async (): Promise<void> => {
-    for (let i = 0; i < 6; i++) {
-        await rotate({ motorIndex: i, degrees: 30 });
-    }
-};
-
 const moveLegForward = async ({
     topMotorIndex,
 }: {
@@ -127,7 +56,7 @@ const moveLegForward = async ({
     await rotate({ motorIndex: topMotorIndex, degrees: 70 });
 };
 
-const moveLegBackwards = async ({
+const moveLegBackward = async ({
     topMotorIndex,
 }: {
     topMotorIndex: number;
@@ -168,19 +97,27 @@ const walk = async (): Promise<void> => {
 
 const execute = async (): Promise<void> => {
     await move({ type: 'reset' });
-    await standUp();
+    // await move({ type: 'standUp' });
     // await moveLegForward({ topMotorIndex: 6 });
+    // await moveLegBackward({ topMotorIndex: 9 });
+    // await moveLegForward({ topMotorIndex: 7 });
+    // await moveLegBackward({ topMotorIndex: 10 });
     // await moveLegForward({ topMotorIndex: 8 });
-    // await moveLegForward({ topMotorIndex: 10 });
-    // await moveLegBackwards({ topMotorIndex: 7 });
-    // await moveLegBackwards({ topMotorIndex: 9 });
-    // await moveLegBackwards({ topMotorIndex: 11 });
+    // await moveLegBackward({ topMotorIndex: 11 });
 
     // await moveLegForward({ topMotorIndex: 7 });
-    // await moveLegBackwards({ topMotorIndex: 10 });
-    // await moveLegBackwards({ topMotorIndex: 6 });
+    // await moveLegForward({ topMotorIndex: 9 });
     // await moveLegForward({ topMotorIndex: 11 });
-    // await moveLegBackwards({ topMotorIndex: 8 });
+
+    // await moveLegBackward({ topMotorIndex: 7 });
+    // await moveLegBackward({ topMotorIndex: 9 });
+    // await moveLegBackward({ topMotorIndex: 11 });
+
+    // await moveLegForward({ topMotorIndex: 7 });
+    // await moveLegBackward({ topMotorIndex: 10 });
+    // await moveLegBackward({ topMotorIndex: 6 });
+    // await moveLegForward({ topMotorIndex: 11 });
+    // await moveLegBackward({ topMotorIndex: 8 });
     // await moveLegForward({ topMotorIndex: 10 });
 };
 void execute();
